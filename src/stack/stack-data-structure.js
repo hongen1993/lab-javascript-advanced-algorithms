6ab78ac2145ff9d8c527b630fe6249669a79c216
hongen <hongen1993@gmail.com>
2022-10-25 18:01:31 +0200

    done

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,25 +5,40 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length < this.MAX_SIZE)
+      return true
+    if (this.stackControl.length === this.MAX_SIZE)
+      return false
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (!this.stackControl.length) {
+      return true
+    } else {
+      return false
+    }
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item)
+      return this.stackControl
+    } else {
+      throw new Error('STACK_OVERFLOW')
+    }
   }
 
   pop() {
-    // ... your code goes here
+    if (!this.isEmpty()) {
+      let remove = this.stackControl.pop()
+      return remove
+    } else
+      throw new Error('STACK_UNDERFLOW')
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl
+  }
 }
-
 // This is required to enable the automated tests, please ignore it.
 if (typeof module !== 'undefined') module.exports = Stack;
